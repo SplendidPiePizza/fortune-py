991c1c4ce1cc8ca0ea557d06f38a481a323f17ce
SplendidPiePizza <165284283+SplendidPiePizza@users.noreply.github.com>
2025-01-18 23:18:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,20 +15,14 @@ def get_random_lines_from_file(file_path, num_lines=1):
     remaining_lines = [line for line in lines if line not in random_lines]
 
     return random_lines, remaining_lines
-
-def save_remaining_lines(file_path, remaining_lines):
-    with open(file_path, 'w') as file:
-        for line in remaining_lines:
-            file.write(line + '\n')
-
 if __name__ == "__main__":
     file_path = 'fortune.txt'
     random_lines, remaining_lines = get_random_lines_from_file(file_path, num_lines=1)
 
     if random_lines:
+    
+    
         for line in random_lines:
             print(line)
-
-        save_remaining_lines(file_path, remaining_lines)
     else:
-        print("missing") # ensure that the lines are not missing, recheck fortune.txt
+        print("missing")
